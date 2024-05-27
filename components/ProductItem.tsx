4cd0295a572d78b36e26465a0af185958ba6b395
Paul Buscano <pbuscano@propertyvista.com>
2024-05-27 20:16:59 +0800

    [ADDED] home label

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Image, Pressable, View } from "react-native";
 import Heading from "./Heading";
 import Paragraph from "./Paragraph";
+import Spacer from "./Spacer";
 
 type Props = {
   item: any;
@@ -11,36 +12,63 @@ type Props = {
 
 const ProductItem = ({ item, index }: Props) => {
   return (
-    <View
-      className={`bg-gray-100 rounded-2xl p-5 justify-between ${
-        index % 2 == 0 ? "mr-2 h-[300px]" : "ml-2 h-[350px]"
-      }`}
-    >
-      <Pressable onPress={() => {}} className="active:opacity-70">
-        <Paragraph textColor="text-black">{item.brand}</Paragraph>
-        <Paragraph textColor="text-black">{item.model}</Paragraph>
-      </Pressable>
-      <Pressable onPress={() => {}} className="active:opacity-70">
-        <Image
-          source={require("../assets/images/s24.png")}
-          className="h-40 w-full"
-        />
-      </Pressable>
-      <View className="flex-row items-center justify-between">
-        <Pressable onPress={() => {}} className="active:opacity-70">
-          <Heading textSize="text-lg">
-            {"\u20B1"}
-            {item.price}
-          </Heading>
-        </Pressable>
-        <Pressable
-          onPress={() => {}}
-          className="active:opacity-70 bg-red-600 rounded-full p-2"
+    <>
+      {item.id === 0 ? (
+        <View className="mr-2 py-5">
+          <Heading textSize="text-2xl">Buy now</Heading>
+          <Heading textSize="text-2xl">Pay Later</Heading>
+          <Spacer height="h-2" />
+          <Paragraph textColor="text-black">
+            Pay our gadgets in Easy Monthly installments with billease
+          </Paragraph>
+        </View>
+      ) : item.id === 3 ? (
+        <View className="h-[100px] bg-red-600 ml-2 rounded-2xl p-3">
+          <Image
+            source={require("../assets/images/bg-label.png")}
+            className="w-20 h-20 absolute self-center top-3 opacity-20"
+          />
+          <Heading textSize="text-lg text-white">New Arrivals</Heading>
+          <Paragraph textColor="text-white text-[11px]">
+            Mobile Phones, Home Appliances
+          </Paragraph>
+          <Paragraph textColor="text-white text-[11px]">
+            Electronics Accessories{" "}
+          </Paragraph>
+        </View>
+      ) : (
+        <View
+          className={`bg-gray-100 rounded-2xl p-5 justify-between ${
+            index % 2 == 0 ? "mr-2 h-[300px]" : "ml-2 h-[350px]"
+          }`}
         >
-          <Feather name="shopping-cart" size={20} color="white" />
-        </Pressable>
-      </View>
-    </View>
+          <Pressable onPress={() => {}} className="active:opacity-70">
+            <Paragraph textColor="text-black">{item.brand}</Paragraph>
+            <Paragraph textColor="text-black">{item.model}</Paragraph>
+          </Pressable>
+          <Pressable onPress={() => {}} className="active:opacity-70">
+            <Image
+              source={require("../assets/images/s24.png")}
+              className="h-40 w-full"
+            />
+          </Pressable>
+          <View className="flex-row items-center justify-between">
+            <Pressable onPress={() => {}} className="active:opacity-70">
+              <Heading textSize="text-lg">
+                {"\u20B1"}
+                {item.price}
+              </Heading>
+            </Pressable>
+            <Pressable
+              onPress={() => {}}
+              className="active:opacity-70 bg-red-600 rounded-full p-2"
+            >
+              <Feather name="shopping-cart" size={20} color="white" />
+            </Pressable>
+          </View>
+        </View>
+      )}
+    </>
   );
 };
 
--- components/pages/HomeLatestProductList.tsx
+++ components/pages/HomeLatestProductList.tsx
@@ -7,49 +7,75 @@ type Props = {};
 
 const DATA = [
   {
+    id: 1,
     brand: "Samsung",
     model: "Galaxy S24",
     price: "87990",
   },
   {
+    id: 2,
     brand: "Samsung",
     model: "Galaxy S24",
     price: "87990",
   },
   {
+    id: 8,
     brand: "Samsung",
     model: "Galaxy S24",
     price: "87990",
   },
   {
+    id: 4,
     brand: "Samsung",
     model: "Galaxy S24",
     price: "87990",
   },
   {
+    id: 5,
     brand: "Samsung",
     model: "Galaxy S24",
     price: "87990",
   },
   {
+    id: 6,
     brand: "Samsung",
     model: "Galaxy S24",
     price: "87990",
   },
 
   {
+    id: 7,
     brand: "Samsung",
     model: "Galaxy S24",
     price: "87990",
   },
   {
+    id: 8,
     brand: "Samsung",
     model: "Galaxy S24",
     price: "87990",
   },
 ];
 
+const NEW_ITEM = {
+  id: 0,
+  brand: "test",
+  model: "yeah",
+  price: "9999",
+};
+
+const THIRD_ITEM = {
+  id: 3,
+  brand: "test",
+  model: "yeah",
+  price: "9999",
+};
+
 const HomeLatestProductList = (props: Props) => {
+  const updatedData = [NEW_ITEM, ...DATA];
+
+  updatedData.splice(3, 0, THIRD_ITEM);
+
   return (
     <View
       style={{
@@ -58,11 +84,12 @@ const HomeLatestProductList = (props: Props) => {
       }}
     >
       <MasonryFlashList
-        data={DATA}
+        data={updatedData}
         numColumns={2}
         renderItem={({ item, index }) => (
           <ProductItem item={item} index={index} />
         )}
+        keyExtractor={(item, index) => index}
         estimatedItemSize={200}
         ItemSeparatorComponent={() => <View className="h-5" />}
         showsVerticalScrollIndicator={false}
